c31a8644f65b3a9e4a034eb747316a08b0141be1
Jon Haddad <jon@jonhaddad.com>
2014-05-07 21:43:05 -0700

    getting vertex template working

--- graph.d
+++ graph.d
@@ -1,7 +1,6 @@
 
 import std.stdio;
 
-
 /// T is the node type.  
 class Vertex(T) {
     
@@ -15,48 +14,56 @@ class Vertex(T) {
     void addProperty(string k, string v) {
 
     }
+    T getKey() {
+        return id;
+    }
 }
 
 
+/// U is the Vertex key type
 class Graph(V : Vertex!U, U) {
-    //Vertex[K] nodes;
+    V[U] nodes;
 
     void put(Vertex!U node) {
-        //auto key = node.getKey();
-        //this.nodes[key] = node;
+        auto key = node.getKey();
+        this.nodes[key] = node;
     }
-    //V get(K key) {
-     //   return this.nodes.get(key, null);
-    //}
+    V get(U key) {
+        return this.nodes.get(key, null);
+    }
 }
 
-class SimpleNode : Vertex!int {
 
-    /// make sure get by key works
-    unittest {
-        auto g = new Graph!(SimpleNode)();
+alias IntVertex = Vertex!int;
+alias StringVertex = Vertex!int;
 
-        //g.put(new SimpleNode(1));
+/// make sure get by key works
+unittest {
+    auto g = new Graph!(IntVertex)();
+    auto n = new IntVertex(1);
+    n.addProperty("test", "value");
 
-        //auto n = g.get(1);
+    g.put(new IntVertex(1));
 
-        // if we request a non existing key we get back null
-        //auto n2 = g.get(2);
-        //assert(n2 is null);
-    }
+    auto n1 = g.get(1);
 
-    unittest {
-        auto g = new Graph!(SimpleNode)();
-        //auto n1 = new SimpleNode(1);
-        //g.put(n1);
-        //auto n2 = new SimpleNode(2);
-        //g.put(n2);
+    assert(n1 !is null);
+    // if we request a non existing key we get back null
+    auto n2 = g.get(2);
+    assert(n2 is null);
+}
+
+unittest {
+    auto g = new Graph!(IntVertex)();
+    //auto n1 = new SimpleNode(1);
+    //g.put(n1);
+    //auto n2 = new SimpleNode(2);
+    //g.put(n2);
 
 
-
-    }
 
 }
+
 
 void main() {
     writeln("Done.");
