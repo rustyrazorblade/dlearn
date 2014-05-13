8ad5076ab995ab7dce286e79d238f9ddc30353dc
Jon Haddad <jon@jonhaddad.com>
2014-05-13 16:02:41 -0700

    delegate example

--- lambda.d
+++ lambda.d
@@ -13,10 +13,25 @@ class Test {
         f();
     }
 
+    
+    // use a delegate to access the previous context 
+    void do_more_stuff() {
+        int a = 1;
+        void f() {
+            a = 2;
+            writeln("HI", a);
+        }
+        do_more_stuff2(&f); 
+    }
+
+    void do_more_stuff2(void delegate () f) {
+        f();
+    }
 }
 
 void main() {
     auto t = new Test();
     t.do_stuff();
+    t.do_more_stuff();
 }
 
